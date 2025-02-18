0e5ff9aacefdf55d59098db670bd0dbbfdcf912a
ingstats <statsxwhiz@gmail.com>
2025-02-18 12:35:40 +0900

    Update model loading method to SavedModel

--- model.py
+++ model.py
@@ -6,7 +6,8 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("saved_model")
+
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
